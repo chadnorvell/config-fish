eb3c6eb5ff4d5177d1e88ff721d44020639d560b
Chad Norvell <chadxnor@gmail.com>
2025-04-29 21:17:57 -0700

    Support direnv

--- config.fish
+++ config.fish
@@ -4,4 +4,7 @@ if type -q vfox
     vfox activate fish | source
 end
 
+if type -q direnv
+    direnv hook fish | source
+
 test -f ~/.orbstack/shell/init2.fish; and source ~/.orbstack/shell/init2.fish
