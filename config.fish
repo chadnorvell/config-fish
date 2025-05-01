c4621230c1413bcc4d9b4042eaef4054a0862d8e
Chad Norvell <chadxnor@gmail.com>
2025-05-01 11:04:09 -0700

    Add opam config

--- config.fish
+++ config.fish
@@ -6,5 +6,9 @@ end
 
 if type -q direnv
     direnv hook fish | source
+end
+
+test -r $HOME/.opam/opam-init/init.fish; \
+    and source $HOME/.opam/opam-init/init.fish > /dev/null 2> /dev/null
 
 test -f ~/.orbstack/shell/init2.fish; and source ~/.orbstack/shell/init2.fish
