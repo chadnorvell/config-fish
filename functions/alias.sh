7793561d622fb6acdacc317224d8de80910cfb52
Chad Norvell <chadxnor@gmail.com>
2025-06-11 21:59:47 -0700

    Add aliases

--- functions/alias.sh
+++ functions/alias.sh
@@ -1,3 +1,11 @@
+function clear --wraps 'clear -x' --description 'alias clear=clear -x'
+  command clear -x $argv
+end
+
+function cp --wraps 'cp -v' --description 'alias cp=cp -v'
+  cp -v $argv
+end
+
 function grep --wraps 'grep --color=auto' --description 'alias grep=grep --color=auto'
  command grep --color=auto $argv
 end
@@ -6,3 +14,10 @@ function ls --wraps 'ls --color=auto' --description 'alias ls=ls --color=auto'
  command ls --color=auto $argv
 end
 
+function rm --wraps 'trash-put' --description 'alias rm=trash-put'
+  trash-put $argv
+end
+
+function rrm --wraps 'rm' --description 'alias rrm=rm'
+  rm $argv
+end
